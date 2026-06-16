e4efd4dbd67633c9468b94d040d0f158e178bbf0
ArthurPereira026 <arthurpereiraoliveira026@gmail.com>
2026-06-16 17:32:28 -0300

    atividade final

---
+++ avaliaçaoFinalUC3.py
@@ -0,0 +1,15 @@
+estoque = {
+    1 : {"Teclado mecânico magnético" : 300},
+    2 : {"Mouse Gamer Redragon" : 450 },
+    3 : {"Monitor Curvo Samsung Odyssey G5 32" : 2000}
+}
+carrinho = []
+
+while True:
+    print("""
+    ----------------------------------------------
+      SEJA BEM-VINDO AO ➡ WIFI EXPRESS STORE 🛜
+    ----------------------------------------------
+            OPÇÕES DISPONIVEIS PRA VOCÊ
+    
+    """)
