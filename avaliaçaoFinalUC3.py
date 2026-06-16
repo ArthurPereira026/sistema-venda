66af8d2b51485f24226ea3a3ba45e1890a55f1ee
ArthurPereira026 <arthurpereiraoliveira026@gmail.com>
2026-06-16 17:42:06 -0300

    Atualização da atividade

--- avaliaçaoFinalUC3.py
+++ avaliaçaoFinalUC3.py
@@ -10,6 +10,10 @@ while True:
     ----------------------------------------------
       SEJA BEM-VINDO AO ➡ WIFI EXPRESS STORE 🛜
     ----------------------------------------------
-            OPÇÕES DISPONIVEIS PRA VOCÊ
-    
-    """)
+           -> OPÇÕES DISPONIVEIS PARA VC <-
+    ----------------------------------------------        
+    [1]  Visualizar Estoque      
+    [2]  Adicionar item ao Carrinho
+    [3]  Visualizar Carrinho
+    [4]  Finalizar Compra
+    [0]  Sair do Sistema """)
